0b35806a2976e3455e5e2adb60622d18f8e6a684
Priyank Verma <priyankvex95@gmail.com>
2016-03-18 01:41:08 +0530

    Create home screen layout

--- app/src/main/java/com/wordpress/priyankvex/quarkmlearn/MainActivity.java
+++ app/src/main/java/com/wordpress/priyankvex/quarkmlearn/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wordpress.priyankvex.quarkmlearn;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.balysv.materialripple.MaterialRippleLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,13 +23,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        ViewGroup layoutAssignments = (ViewGroup) findViewById(R.id.layoutAssignments);
+        MaterialRippleLayout.on(layoutAssignments)
+                .rippleColor(R.color.colorAccent)
+                .rippleHover(true)
+                .rippleAlpha(0.1f)
+                .rippleOverlay(true)
+                .create();
+        layoutAssignments.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Assignments", Toast.LENGTH_SHORT).show();
             }
         });
     }
